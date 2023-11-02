c374af914c38d755566f3d44fb842e398caa2afa
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-11-02 13:04:17 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { loadOff, loadOn } from './js/loadState';
 import { errorFn } from './js/errors';
 import SlimSelect from 'slim-select';
 import '/node_modules/slim-select/dist/slimselect.css';
+loadOn();
 
 refs.listEl.addEventListener('change', evt => {
   refs.imgContainerEl.classList.add('hidden');
@@ -25,9 +26,7 @@ refs.listEl.addEventListener('change', evt => {
 
 fetchBreeds()
   .then(data => {
-    loadOff();
     const markup = selectMarkup(data.data);
-
     refs.listEl.insertAdjacentHTML(
       'beforeend',
       `<option data-placeholder="true"></option>${markup}`
@@ -39,8 +38,9 @@ fetchBreeds()
         placeholderText: 'Вибери котика',
       },
     });
+    loadOff();
   })
   .catch(error => {
     errorFn(error.message);
     loadOn();
-  });
+  })
